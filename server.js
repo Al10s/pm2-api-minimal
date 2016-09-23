183435aa27f98d920fc04dd79a8a843cc0ad1c8b
Antoine LEMARCHAND <an.lemarchand@gmail.com>
2016-09-23 12:04:38 +0200

    Added the binding to the pm2 instance to pm2.connect, fixing the behavior in the 2.0+ pm2 versions

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express');
 var app = express();
 
 app.get('/pm2/list', function (req, res) {
-	Q.nbind(pm2.connect)()
+	Q.nbind(pm2.connect, pm2)()
 	.then(function () {
 		return Q.nbind(pm2.list, pm2)()
 		.then(function (informations) {
